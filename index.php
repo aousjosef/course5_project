da02a2e242f882d864b8cd5b5b8bf8cf7afee770
aousjosef <aoussabah@gmail.com>
2023-03-04 22:21:17 +0100

    holy-grail using Grid-layout, not fully responsive

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="main_style.css">
+    <title>Aous - Blogg</title>
+</head>
+
+<body>
+
+    <div class="holy-grid">
+
+        <header>
+
+
+        </header>
+
+
+        <nav>
+
+
+        </nav>
+
+
+        <main>
+
+
+        </main>
+
+
+
+        <aside>
+
+
+        </aside>
+
+
+
+
+        <footer>
+
+
+        </footer>
+
+    </div>
+</body>
+
+</html>
