70051af2001379844fa021867ecc9e32e193af97
aousjosef <aoussabah@gmail.com>
2023-03-05 03:17:09 +0100

    created holy grail CSS and HTML, flexbox, colors

--- index.php
+++ index.php
@@ -6,42 +6,48 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="main_style.css">
-    <title>Aous - Blogg</title>
+    <title>Projekt - blogg</title>
 </head>
 
 <body>
 
-    <div class="holy-grid">
-        THIS BRANCH
+    <div class="holy-grid-flexcontianer">
+
         <header>
-
-
+            Header
         </header>
 
 
-        <nav>
+        <div class="flex-trinity-container">
+
+            <nav>
+
+                main nav
+
+            </nav>
 
 
-        </nav>
+            <main>
 
-
-        <main>
-
-
-        </main>
+                main content
+            </main>
 
 
 
-        <aside>
+            <aside>
+
+                sub nav -aside
+            </aside>
 
 
-        </aside>
+        </div>
+
 
 
 
 
         <footer>
-
+            footer
 
         </footer>
 
