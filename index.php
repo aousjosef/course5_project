b34cf401c3473c6816917f8699b3ba4257d56174
aousjosef <aoussabah@gmail.com>
2023-03-06 19:26:22 +0100

    home page style update

--- index.php
+++ index.php
@@ -6,6 +6,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="main_style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
     <title>Projekt - blogg</title>
 </head>
 
@@ -14,7 +17,11 @@
     <div class="holy-grid-flexcontianer">
 
         <header>
-            Header
+            <div class="logo-img-container">
+                Logos
+            </div>
+
+            <a href="" class="login">Log in</a>
         </header>
 
 
@@ -22,14 +29,29 @@
 
             <nav>
 
-                main nav
+                <ul class="unord-list-container">
+
+                    <li><a href="">Startsida</a></li>
+                    <li><a href="">Bloggar</a></li>
+                    <li><a href="">Bli medlem</a></li>
+                    <li><a href="">Mina inlägg</a></li>
+                    <li><a href="">Om sidan</a></li>
+
+
+                </ul>
+
+
 
             </nav>
 
 
+
+
+
+
             <main>
 
-                main content
+                main contentttt
             </main>
 
 
